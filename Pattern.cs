c99d7d6d1d1e2b0b2145bc9a54f893d853a195b4
himanshu01031997 <hs010397@gmail.com>
2022-11-03 21:48:23 +0530

    add:firstname

---
+++ Pattern.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUsingLamdaDay24
+{
+    public class Pattern
+    {
+        public const String FIRST_NAME = "^[A-Z]{1}[a-zA-Z]{2,}$";
+        public static Func<string, string> checkfirstname = (firstname) =>//func delegate which accept <input ,output>
+        {
+            Regex regex = new Regex(FIRST_NAME);
+            if (regex.IsMatch(firstname))
+                return "firstname is valid";
+            else
+                return "firstname is invalid";
+
+        };
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace UserRegistrationUsingLamdaDay24
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the first name:");
+            string firstname = Console.ReadLine();
+            string result = Pattern.checkfirstname(firstname);
+            Console.WriteLine(result);
+        }
+    }
+}
